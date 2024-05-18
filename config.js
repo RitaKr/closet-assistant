eb002342fe1a551feeb591463be4957117789261
RitaKr <ritokra@gmail.com>
2024-05-18 20:22:58 +0300

    configuring project

---
+++ config.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+
+    projectId: process.env.FIREBASE_PROJECT_ID,
+
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+
+    appId: process.env.FIREBASE_APP_ID,
+
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    databaseURL: process.env.FIREBASE,
+  
+    
+  };
+
+const weatherApiKey = process.env.WEATHER_API_KEY;
+const clipdropApiKey = process.env.CLIPDROP_API_KEY;
+export{firebaseConfig, weatherApiKey, clipdropApiKey}
